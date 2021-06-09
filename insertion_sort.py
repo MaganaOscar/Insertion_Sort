5ca4fadd8c865b13a89d0d4c35cb69c3f1cbd266
MaganaOscar <oscmag13@gmail.com>
2021-06-08 19:37:53 -0700

    removed val

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,5 @@
 def insertion_sort(list):
     for index in range(1, len(list)):
-        val = list[index - 1]
         for check in range(index, 0, -1):
             if list[check] < list[check-1]:
                 list[check], list[check - 1] = list[check - 1], list[check]
